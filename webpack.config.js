6339304fbd7dac23a4375574f2f4f0ff73b8b83d
yegecali <yegecali2@gmail.com>
2022-12-03 13:05:00 -0500

    feat: #1, #2, #3 setup webpack pug and scss

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+//webpack.config.js
+const path = require('path');
+const PugPlugin = require('pug-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/pug/pages/index.pug'
+    //☝🏽 Insert your PUG HTML files here
+  },
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: 'assets/js/[name].[contenthash:8].js'
+    //☝🏽 Output filename of files with hash for unique id
+  },
+  plugins: [
+    new PugPlugin({
+      pretty: true,
+      //☝🏽 Format HTML (only in dev mode)
+      extractCss: {
+        filename: 'assets/css/[name].[contenthash:8].css'
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: PugPlugin.loader
+        //☝🏽 Load Pug files
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: ['css-loader', 'sass-loader']
+        //☝🏽 Load Sass files
+      },
+      {
+        // To use images on pug files:
+        test: /\.(png|jpg|jpeg|ico)/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/img/[name].[hash:8][ext]'
+        }
+      },
+      {
+        // To use fonts on pug files:
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext][query]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    watchFiles: {
+      paths: ['src/**/*.*', 'src/scss/**/*.*'],
+      //☝🏽 Enables HMR in these folders
+      options: {
+        usePolling: true
+      }
+    }
+  },
+  stats: 'errors-only'
+  //☝🏽 For a cleaner dev-server run
+};
